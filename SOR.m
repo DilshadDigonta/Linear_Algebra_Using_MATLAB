b35b4de220f9205220e59dda34fa979ee3cdd068
DilshadDigonta <91944205+DilshadDigonta@users.noreply.github.com>
2021-10-06 12:11:47 +0600

    Add files via upload

---
+++ SOR.m
@@ -0,0 +1,33 @@
+%SOR Method
+
+A=input('Please Enter the Co efficient Mattrix:' ); 
+b=input('Please Enter the constant:'); 
+m=size(A,1);
+ig = input('Please enter initial guesses');
+for i=1:m
+    x(i)=ig(i); xold(i)=x(i);
+end
+sum=0; 
+err=0;
+w = input('Relaxation Factor w =');
+for k=1:1000 % k is the number of iterations
+    for i=1:m
+        for j=1:m
+            if j~=i
+                sum=sum+A(i,j)*x(j);
+            end
+        end
+        x(i)=(1-w)*x(i)+(w*(b(i)-sum)/A(i,i));
+        sum=0.0;
+        err=err+abs((x(i)-xold(i))/x(i));
+        xold(i)=x(i);
+    end
+    if err<=0.0000001 break ; end
+    err=0.0;
+end
+fprintf('Solve for X in SOR: \n')
+for i=1:m 
+    disp (x(i));
+end
+fprintf('Number of iteration:')
+disp (k);
